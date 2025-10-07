545c7bfb784d244d21454266ce36176df2667a83
TAN DAT <dattt1848@ut.edu.vn>
2025-10-08 01:21:24 +0700

    Tao cau truc du an va cau hinh co ban

---
+++ src/main/java/com/web/medicine/baoanmedicine/BaoAnMedicineApplication.java
@@ -0,0 +1,13 @@
+package com.web.medicine.baoanmedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoAnMedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoAnMedicineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/medicine/baoanmedicine/BaoAnMedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.medicine.baoanmedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoAnMedicineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
